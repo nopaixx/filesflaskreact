10dc407aa250b6885e17acfc476d36736f7bbce6
angel <nopaixx@gmail.com>
2020-02-23 22:12:03 +0100

    Final release pending test

---
+++ backend/test/test_images_middelware.py
@@ -0,0 +1,15 @@
+# TODO
+
+# Estas funciones son las que de deberían testeat, y se hace una cobertura casi total
+# del aplicativo, ademas son faciles de testear porque la mayoria sn agnositcas
+# y realizan una sola funcion
+
+save_file
+generate_name
+generate_path
+get_secure_name
+valid_request
+allowed_file
+valid_file
+virus_scan
+valid_name
